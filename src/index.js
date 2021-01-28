5ec1f57c597d2396de2d1979ca5581675a1f377c
Robert Smith <rsmithlal@gmail.com>
2021-01-27 23:08:30 -0330

    Change app index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import BtApp from './BtApp.vue'
 
-export default {
-  install(Vue, options) {
-    Vue.component('BtApp', BtApp)
-  },
+const NAME = 'CommunityEngineVue'
+
+const install(Vue) {
+  // Vue.component('BtApp', BtApp)
 }
+
+// --- CommunityEngineVue plugin ---
+const CommunityEngineVue = /*#__PURE__*/ {
+  install,
+  NAME
+}
+
+console.log('test')
+
+export {
+  install,
+  NAME,
+  CommunityEngineVue
+}
+
+export { BtNavUser } from './components/BtNavUser'
+
+// Default export is the CommunityEngineVue plugin
+export default CommunityEngineVue
