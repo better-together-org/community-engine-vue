a431d5d4318d9b44915a5e46a332ff72c9af8a6d
Robert Smith <rsmithlal@gmail.com>
2021-01-27 23:38:07 -0330

    Get package import working

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import BtApp from './BtApp.vue'
+import BtNavUser from './components/BtNavUser.vue'
 
 const NAME = 'CommunityEngineVue'
 
-const install(Vue) {
-  // Vue.component('BtApp', BtApp)
+const install = (Vue) => {
+  Vue.component('BtNavUser', BtNavUser)
 }
 
 // --- CommunityEngineVue plugin ---
-const CommunityEngineVue = /*#__PURE__*/ {
+const CommunityEngineVue = /* #__PURE__ */ {
   install,
-  NAME
+  NAME,
 }
 
-console.log('test')
+console.log('Loading Community Engine')
 
 export {
   install,
   NAME,
-  CommunityEngineVue
+  CommunityEngineVue,
 }
 
-export { BtNavUser } from './components/BtNavUser'
+export { BtNavUser }
 
 // Default export is the CommunityEngineVue plugin
 export default CommunityEngineVue
