ba36a986512a6a862b003c8ed978234212897992
Robert Smith <rob@bettertogethersolutions.com>
2023-10-29 16:05:58 -0230

    Remove deprecated Vuex Persisted State
    Solves security issue with SecureLs dependency in unmaintained lib

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
-import SecureLS from 'secure-ls'
 import CommunityEngine from './modules/community-engine'
-
-const ls = new SecureLS({ isCompression: false })
 
 Vue.use(Vuex)
 
@@ -12,15 +8,7 @@ const BtStoreModules = {
   CommunityEngine,
 }
 
-const BtStorePlugins = [
-  createPersistedState({
-    storage: {
-      getItem: (key) => ls.get(key),
-      setItem: (key, value) => ls.set(key, value),
-      removeItem: (key) => ls.remove(key),
-    },
-  }),
-]
+const BtStorePlugins = []
 
 const BtStore = new Vuex.Store({
   modules: BtStoreModules,
